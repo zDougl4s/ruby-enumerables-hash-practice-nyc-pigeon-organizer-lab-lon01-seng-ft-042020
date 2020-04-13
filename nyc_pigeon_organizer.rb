5c838619a194667e76c038ceb9abe4c69cee6d56
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-13 18:02:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  # subway = data[:lives]["Subway"]
+  pigeon_list = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, names|
+      names.each do |name|
+      if pigeon_list[name] == nil
+        pigeon_list[name]={}
+      end
+      if pigeon_list[name][color_gender_lives] == nil
+        pigeon_list[name][color_gender_lives] = []
+      end
+        pigeon_list[name][color_gender_lives]<< stats.to_s
+      end
+    end
+  end
+  pigeon_list
 end
