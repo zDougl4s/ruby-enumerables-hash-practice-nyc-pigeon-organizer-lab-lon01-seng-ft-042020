0b5b30edf4a7d96adc6ed96eb3a79b519d8b41f2
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-13 16:31:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   # subway = data[:lives]["Subway"]
   lala = data.map do |i|
-    i == :color
+    i == i[:color]
   end
   binding.pry
 end
