50acc35e6630899af39f8261e5f5ff15fa2ff676
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-13 17:13:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,18 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   # subway = data[:lives]["Subway"]
+  pigeon_list = {}
   data.each do |color_gender_lives, value|
     value.each do |stats, names|
       names.each do |name|
-        name
+      if pigeon_list[name] == nil
+        pigeon_list << name
         binding.pry
       end
     end
   end
 end
+
+lala = {
+  "javier": {}
+}
